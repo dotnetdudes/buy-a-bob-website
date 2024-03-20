e71d5b58adeb0161aba734510e0d94fb32ae815c
John Morton <johnmorton503@gmail.com>
2024-03-20 16:04:30 +0800

    Add back button

--- src/features/gallery/admin/GalleryNew/GalleryNew.jsx
+++ src/features/gallery/admin/GalleryNew/GalleryNew.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
-import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import BackButton from "../../../../components/BackButton";
 import TextField from "@mui/material/TextField";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from "@mui/icons-material/Save";
 import { useState } from "react";
 import Product from "../../../../services/dudesApi/classes/Product";
 import Paper from "@mui/material/Paper";
@@ -9,9 +12,12 @@ import Grid from "@mui/material/Grid";
 import { useDispatch } from "react-redux";
 import { fetchToken } from "../../../antiforgery/slice";
 import { addProduct } from "../../../products/slice";
+import { selectProductState } from "../../../products/slice/selectors";
+import { useSelector } from "react-redux";
 
 const GalleryNew = () => {
   const dispatch = useDispatch();
+  const { loading } = useSelector(selectProductState);
   const [formData, setFormData] = useState(new Product());
 
   useEffect(() => {
@@ -30,7 +36,7 @@ const GalleryNew = () => {
     }
     // attach file
     postData.append("file", artwork, artwork.name);
-    
+
     dispatch(addProduct(postData));
   };
 
@@ -42,42 +48,41 @@ const GalleryNew = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Paper elevation={3} style={{ padding: 20, marginBottom: 20 }}>
-          <Typography variant="h5" gutterBottom>
-            Add artwork
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              label="Name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Price"
-              name="price"
-              type="number"
-              value={formData.price}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Description"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <fieldset>
-                <legend>Upload Image</legend>
+      <Grid item xs={12} sx={{ mb: 4 }}>
+        <Typography variant="h5" gutterBottom>
+          Add artwork
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Price"
+            name="price"
+            type="number"
+            value={formData.price}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <fieldset>
+            <legend>Upload Image</legend>
             <TextField
               name="imageUrl"
               type="file"
@@ -85,63 +90,71 @@ const GalleryNew = () => {
               fullWidth
               margin="normal"
             />
-            </fieldset>
-            <TextField
-              label="Weight"
-              name="weight"
-              type="number"
-              value={formData.weight}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Width"
-              name="width"
-              type="number"
-              value={formData.width}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Depth"
-              name="depth"
-              type="number"
-              value={formData.depth}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Height"
-              name="height"
-              type="number"
-              value={formData.height}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            <TextField
-              label="Quantity"
-              name="quantity"
-              type="number"
-              value={formData.quantity}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
-            {/* You can add more fields here */}
-            <Button type="submit" variant="contained" color="primary">
-              Submit
-            </Button>
-          </form>
-        </Paper>
+          </fieldset>
+          <TextField
+            label="Weight"
+            name="weight"
+            type="number"
+            value={formData.weight}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Width"
+            name="width"
+            type="number"
+            value={formData.width}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Depth"
+            name="depth"
+            type="number"
+            value={formData.depth}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Height"
+            name="height"
+            type="number"
+            value={formData.height}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Quantity"
+            name="quantity"
+            type="number"
+            value={formData.quantity}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <ButtonGroup variant="contained" aria-label="Basic button group">
+            <LoadingButton
+              color="secondary"
+              type="submit"
+              loading={loading}
+              loadingPosition="start"
+              startIcon={<SaveIcon />}
+              variant="contained"
+            >
+              <span>Save</span>
+            </LoadingButton>
+            <BackButton />
+          </ButtonGroup>
+        </form>
       </Grid>
     </Grid>
   );
